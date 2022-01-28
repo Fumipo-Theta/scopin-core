565ffee5bd2c683d274025a567cf60a09d611780
Fumitoshi Morisato <fmorisato@gmail.com>
2022-01-28 22:56:25 +0900

    Fix type of sample owner bug (#2)

--- vender/component/footer/default/footer.tsx
+++ vender/component/footer/default/footer.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 
+const style = {
+    display: "flex",
+    justifyContent: "center",
+}
+
 const Footer: React.FC = () => {
-    return <></>
+    return <div style={style}><a href="./make_package.html">Sample package maker</a></div>
 }
 
 export default Footer
